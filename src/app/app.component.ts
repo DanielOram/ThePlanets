c8716d66de8e7ec1c629fa2501d6494bd7727718
Daniel Oram <danieloramnz@gmail.com>
2016-12-21 05:10:35 +1300

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Planets, PlanetsService } from './shared';
 
 @Component({
   selector: 'Planets-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Planets works!';
+export class AppComponent implements OnInit {
+  //title = 'Planets works!';
+  planetsList: Planets[] = [];
+  constructor(private _planetservice: PlanetsService) {}
+
+  ngOnInit(){
+    this._planetservice.getPlanets().then(planets => this.planetsList = planets);
+  }
 }
