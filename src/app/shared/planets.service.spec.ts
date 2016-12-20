ff31812922fd4e15e2180095f5f8c623e0309af4
Daniel Oram <danieloramnz@gmail.com>
2016-12-21 04:55:31 +1300

    update comment

--- src/app/shared/planets.service.spec.ts
+++ src/app/shared/planets.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-//create planet service
+//create planet service - this file is for Unit testing planets.service.ts
 //ng generate service shared/planets
 
 import { TestBed, async, inject } from '@angular/core/testing';
