6ffe12f467c832e461d9abd5e3737d4244178fff
Daniel Oram <danieloramnz@gmail.com>
2016-12-22 04:42:29 +1300

    serve static files in /client/dist

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use( express.static(__dirname + '/client' ) );
+//app.use( express.static(__dirname + '/client/dist' ));
 
 var listener = server.listen(process.env.PORT || 5000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000
