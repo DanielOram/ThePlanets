10fd3b7a74f1cc95774d2d92f8a4064b3f54ad78
Daniel Oram <danieloramnz@gmail.com>
2016-12-25 03:07:40 +1300

    add route for index page

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var app = express();
 var server = require('http').Server(app);
 var bodyParser = require('body-parser');
 
+//routes
+var index = require('./routes/index');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use( express.static(__dirname + '/client/dist' ) );
+
+app.use('/', index);
 
 var listener = server.listen(process.env.PORT || 5000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  //res.render('index');
+  //res.render('hello world')
+  res.send('this is the index page');
 });
 
 module.exports = router;
