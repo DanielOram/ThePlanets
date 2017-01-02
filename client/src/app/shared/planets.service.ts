f49b84a5eddcb7d3780053ed2aeb3fbc1dab2b5f
Daniel Oram <danieloramnz@gmail.com>
2017-01-03 12:44:45 +1300

    update planet service to include images

--- client/src/app/shared/planets.service.ts
+++ client/src/app/shared/planets.service.ts
@@ -14,14 +14,15 @@ export class PlanetsService {
   }
 }
 
+const IMG_SRC: string = 'assets/images/';
 
 const PLANETSDATA: Planets[] = [
-  {position: 1, name: 'Mercury', distanceFromSun: 58, description: '88 earth days to orbit the sun' },
-  {position: 2, name: 'Venus', distanceFromSun: 108, description: '225 earth days to orbit the sun' },
-  {position: 3, name: 'Earth', distanceFromSun: 150, description: '365 earth days to orbit the sun' },
-  {position: 4, name: 'Mars', distanceFromSun: 228, description: '686 earth days to orbit the sun' },
-  {position: 5, name: 'Jupiter', distanceFromSun: 778, description: '12 earth years to orbit the sun' },
-  {position: 6, name: 'Saturn', distanceFromSun: 886, description: '29 eath years to orbit the sun' },
-  {position: 7, name: 'Uranus', distanceFromSun: 1800, description: '84 earth years to orbit the sun' },
-  {position: 8, name: 'Neptune', distanceFromSun: 2800, description: '165 earth years to orbit the sun' }
+  {position: 1, name: 'Mercury', distanceFromSun: 58, description: '88 earth days to orbit the sun', image: IMG_SRC + 'mercury.jpg' },
+  {position: 2, name: 'Venus', distanceFromSun: 108, description: '225 earth days to orbit the sun', image: IMG_SRC + 'venus.jpg' },
+  {position: 3, name: 'Earth', distanceFromSun: 150, description: '365 earth days to orbit the sun', image: IMG_SRC + 'earth.jpg' },
+  {position: 4, name: 'Mars', distanceFromSun: 228, description: '686 earth days to orbit the sun', image: IMG_SRC + 'mars.jpg' },
+  {position: 5, name: 'Jupiter', distanceFromSun: 778, description: '12 earth years to orbit the sun', image: IMG_SRC + 'jupiter.jpg' },
+  {position: 6, name: 'Saturn', distanceFromSun: 886, description: '29 eath years to orbit the sun', image: IMG_SRC + 'saturn.jpg' },
+  {position: 7, name: 'Uranus', distanceFromSun: 1800, description: '84 earth years to orbit the sun', image: IMG_SRC + 'uranus.jpg' },
+  {position: 8, name: 'Neptune', distanceFromSun: 2800, description: '165 earth years to orbit the sun', image: IMG_SRC + 'neptune.jpg' }
 ]
