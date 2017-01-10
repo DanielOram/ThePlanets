a103668ce64d5bf95e576188b500a0dd0e8aecc0
Daniel Oram <danieloramnz@gmail.com>
2017-01-10 18:34:26 +1300

    update planets model

--- client/src/app/shared/planets.ts
+++ client/src/app/shared/planets.ts
@@ -5,7 +5,12 @@ export class Planets {
     position: number;
     name: string;
     distanceFromSun: number;
-    yearLength: number;
+    yearLength: string;
     description: string;
     image: string;
+    diameter_km: number;
+    temperature_low: string; 
+    temperature_high: string;
+    temperature_avg: string;  
+    gravity: number; 
 }
