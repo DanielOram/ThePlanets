13f26e8d2edc1002acdf0d09cd298a38b8cb27e6
Daniel Oram <danieloramnz@gmail.com>
2017-01-10 18:35:00 +1300

    add constants file

---
+++ client/src/app/constants.ts
@@ -0,0 +1,9 @@
+export module Constants {
+
+    //planet image folder
+    const IMG_SRC: string = 'assets/images/';
+    
+    export function getImgSrc(): string {
+        return IMG_SRC;
+    }
+}
